281298741ff9d8131bb1b85aad4276e68c00af99
git@github.com:takuto0414/wildman.git <tamoumi0414@gmail.com>
2021-03-19 10:14:43 +0900

    .

--- tentyu/functions/api/customer.js
+++ tentyu/functions/api/customer.js
@@ -10,15 +10,28 @@ exports.handler = async function(event) {
 
     const customer = await stripe.customers.create(customers);
 
-    console.log("customer", customer);
+    exports.handler = async function(event) {
+        const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-    const headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type"
+        const data = JSON.parse(event.body);
+        // TODO 登録メールへ変更
+        const customers = {
+            email: "customer1@example.com",
+            source: data.token
+        };
+
+        const customer = await stripe.customers.create(customers);
+
+        console.log("customer", customer);
+
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type"
+        };
+        return {
+            statusCode: 200,
+            headers,
+            body: result ? "NORMAL" : "ERROR"
+        };
     };
-    return {
-        statusCode: 200,
-        headers,
-        body: result ? "NORMAL" : "ERROR"
-    };
-};
+}
--- tentyu/webpack.functions.js
+++ tentyu/webpack.functions.js
@@ -1,5 +1,5 @@
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  plugins: [new Dotenv()]
+    plugins: [new Dotenv()]
 };
